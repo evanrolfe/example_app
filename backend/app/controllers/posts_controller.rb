a6353b6ce4c9bdf8f4c3bf63da55232ac78e847e
Evan Rolfe <evanrolfe@gmail.com>
2019-06-20 10:32:55 +0100

    Backend: comment auth in controllers

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  #before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  protect_from_forgery unless: -> { request.format.json? }
 
   # GET /posts
   # GET /posts.json
@@ -20,7 +22,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    authorize @post, :update?
+    #authorize @post, :update?
   end
 
   # POST /posts
@@ -42,7 +44,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    authorize @post, :update?
+    #authorize @post, :update?
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -57,7 +59,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    authorize @post, :destroy?
+    #authorize @post, :destroy?
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
@@ -73,6 +75,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body).merge(user_id: current_user.id)
+      params.require(:post).permit(:title, :body)#.merge(user_id: current_user.id)
     end
 end
