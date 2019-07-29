a25d6fe42b22d68df967a1f58edc1684fc8f3345
Evan Rolfe <evanrolfe@gmail.com>
2019-07-29 15:37:49 +0100

    Use database session store

--- backend/db/migrate/20190626142401_change_user_id_to_string.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUserIdToString < ActiveRecord::Migration[5.2]
-  def change
-    change_column(:posts, :user_id, :string)
-    change_column(:secrets, :user_id, :string)
-  end
-end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_142401) do
+ActiveRecord::Schema.define(version: 2019_07_29_143344) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -23,9 +23,18 @@ ActiveRecord::Schema.define(version: 2019_06_26_142401) do
   create_table "secrets", force: :cascade do |t|
     t.string "name"
     t.text "value"
-    t.string "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
