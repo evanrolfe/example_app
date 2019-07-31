c1056ce5f63cb392cb7fc9f280dbc71e2234deb8
Evan Rolfe <evanrolfe@gmail.com>
2019-07-31 12:08:45 +0100

    Add seeds, readme and fix frontend docker

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 *Installation*
 
 ```bash
-$ git clone ...
+$ git clone git@github.com:evanrolfe/example_app.git
 $ cd example_app
-$ docker-compose build
 $ docker-compose up
+$ docker-compose exec backend rake db:setup
 ```
 
 Access the SPA from http://localhost and the MPA from http://localhost:3001.
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,8 +1,8 @@
 # All the passwords for users are just "password"
 User.create!([
-  {email: "alice@authcov.io", name: "Alice", encrypted_password: "$2a$11$ej6BA2.v4.bxbifV.1P3C.cTvv9V9bEE3ajHAiUUOoNJi5oEhL80S", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "bob@authcov.io", name: "Bob", encrypted_password: "$2a$11$cFl5ZV6W9GLDc.r9sqZf6uLbHeNrevnDYWJ9gdsmlQ.KSZp2JCUQO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
-  {email: "celine@authcov.io", name: "Celine", encrypted_password: "$2a$11$FGOSwjEbHlc7SXqABUf5M.Ule.ciDKrx7hJ/DyZOc.yDIik0QsRVq", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+  {email: "alice@authcov.io", name: "Alice", password: 'password', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
+  {email: "bob@authcov.io", name: "Bob", password: 'password', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil},
+  {email: "celine@authcov.io", name: "Celine", password: 'password', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
 ])
 Post.create!([
   {title: "Hello", body: "World", user_id: 1},
